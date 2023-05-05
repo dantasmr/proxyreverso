50c98f5fac901ed46b793ba209f6d9abf77f415a
Marcelo Dantas <dantasmr@yahoo.com.br>
2023-05-04 22:29:31 -0300

    spring

---
+++ springproxy.java
@@ -0,0 +1,17 @@
+@SpringBootApplication
+public class ReverseProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReverseProxyApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .build();
+    }
+}
